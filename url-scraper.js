1ff0c16fbd11373baffad7bcecff954e0b940d03
khanalasim <asim.khanal@vanderbilt.edu>
2016-04-09 17:38:50 -0500

    see

--- url-scraper.js
+++ url-scraper.js
@@ -3,8 +3,16 @@ var sources = [];
 for (var i in imageTags) {
    var src = imageTags[i].src;
    sources.push(src);
+   getMeta(src);
+
 }
 
+function getMeta(url){
+    $("<img/>",{
+        load : function(){ alert(this.width+' '+this.height); },
+        src  : url
+    });
+}
 console.log(sources);
 console.log("test");
 
