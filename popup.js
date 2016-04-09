44df153bb3c69577ae938cf8a5b8d70935e586b4
khanalasim <asim.khanal@vanderbilt.edu>
2016-04-09 16:37:32 -0500

    juts to pull

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
-$('#search').keydown(function(){
-  if(working){
+// $('#search').keydown(function(){
+//   if(working){
 
-  }else{
-    $("#error").text("Scanning images...");
-  }
-})
+//   }else{
+//     $("#error").text("Scanning images...");
+//   }
+// })
 
 
 
@@ -103,42 +103,42 @@ $('#search').keydown(function(){
 //   x.send();
 // }
 
-// function renderStatus(statusText) {
-//   document.getElementById('status').textContent = statusText;
-// }
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   getCurrentTabUrl(function(url) {
-//     // Put the image URL in Google search.
-//     //renderStatus('Performing Google Image search for ' + url);
+document.addEventListener('DOMContentLoaded', function() {
+  getCurrentTabUrl(function(url) {
+    // Put the image URL in Google search.
+    //renderStatus('Performing Google Image search for ' + url);
 
-//     // getImageUrl(url, function(imageUrl, width, height) {
+    // getImageUrl(url, function(imageUrl, width, height) {
 
-//     //   renderStatus('Search term: ' + url + '\n' +
-//     //       'Google image search result: ' + imageUrl);
-//     //   var imageResult = document.getElementById('image-result');
-//     //   // Explicitly set the width/height to minimize the number of reflows. For
-//     //   // a single image, this does not matter, but if you're going to embed
-//     //   // multiple external images in your page, then the absence of width/height
-//     //   // attributes causes the popup to resize multiple times.
-//     //   imageResult.width = width;
-//     //   imageResult.height = height;
-//     //   imageResult.src = imageUrl;
-//     //   imageResult.hidden = false;
+    //   renderStatus('Search term: ' + url + '\n' +
+    //       'Google image search result: ' + imageUrl);
+    //   var imageResult = document.getElementById('image-result');
+    //   // Explicitly set the width/height to minimize the number of reflows. For
+    //   // a single image, this does not matter, but if you're going to embed
+    //   // multiple external images in your page, then the absence of width/height
+    //   // attributes causes the popup to resize multiple times.
+    //   imageResult.width = width;
+    //   imageResult.height = height;
+    //   imageResult.src = imageUrl;
+    //   imageResult.hidden = false;
 
-//     // }, function(errorMessage) {
-//     //   renderStatus('Cannot display image. ' + errorMessage);
-//     // });
+    // }, function(errorMessage) {
+    //   renderStatus('Cannot display image. ' + errorMessage);
+    // });
 
-//     window.onkeyup = keyup;
+    window.onkeyup = keyup;
 
-// 	//creates a global Javascript variable
-// 	var inputTextValue;
+	//creates a global Javascript variable
+	var inputTextValue;
 
-// 	function keyup(e) {
-//   //setting your input text to the global Javascript Variable for every key press
-//   		inputTextValue = e.target.value;
-//   		renderStatus(inputTextValue);
-// 	}
-//   });
-// });
+	function keyup(e) {
+  //setting your input text to the global Javascript Variable for every key press
+  		inputTextValue = e.target.value;
+  		renderStatus(inputTextValue);
+	}
+  });
+});
