1f59f693aefc703cc7be0c54bc6b699ca601c5bf
Hunter Manhart <Hunter.M.Manhart@vanderbilt.edu>
2016-04-09 21:53:04 -0500

    ajax init for google api

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-console.log("can run multiple");
+var key = "";
+
+$.ajax({
+    url: "https://vision.googleapis.com/v1/images:annotate?key=" + key,    
+    type: "GET",    
+    success: function (data) {
+        console.log(data);
+        $.cookie('imageTags', JSON.stringify(data));
+        result = JSON.stringify(data);
+    },
+      error: function(data){
+        console.log("AJAX error: " + data);
+    }
+  }); 
