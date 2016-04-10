fb93f3f5409e811cffe965e7264a9c6e17f5f51e
Patrick Pei <patrickkerry.pei@gmail.com>
2016-04-09 21:54:18 -0500

    Method to convert img url to base64

--- test.js
+++ test.js
@@ -12,3 +12,14 @@ $.ajax({
         console.log("AJAX error: " + data);
     }
   }); 
+
+
+function getBase64Image(imgElem) {
+    var canvas = document.createElement("canvas");
+    canvas.width = imgElem.clientWidth;
+    canvas.height = imgElem.clientHeight;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(imgElem, 0, 0);
+    var dataURL = canvas.toDataURL("image/png");
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
